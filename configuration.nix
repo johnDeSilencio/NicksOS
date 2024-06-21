9ae1f5fb9305ca89b30cdc81905865e6c67d273e
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2024-06-20 22:25:08 -0700

    Add wget

--- configuration.nix
+++ configuration.nix
@@ -133,7 +133,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
+    pkgs.wget
     pkgs.wezterm
     pkgs.git
     pkgs.git-credential-oauth
