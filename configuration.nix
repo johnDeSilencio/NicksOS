f1db31f696da23a843fdbeed192de1d83c1b79f2
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2024-08-13 21:12:49 -0700

    Add Alacritty terminal

--- configuration.nix
+++ configuration.nix
@@ -135,6 +135,7 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     pkgs.wget
     pkgs.wezterm
+    pkgs.alacritty
     pkgs.git
     pkgs.git-credential-oauth
     pkgs.keepassxc
