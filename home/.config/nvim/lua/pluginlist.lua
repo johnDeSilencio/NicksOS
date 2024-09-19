d98e0bf9f4901f7633cc911b7b9bfed1aa599785
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2024-09-18 23:49:01 -0700

    Flesh out basic NeoVim configuration

--- home/.config/nvim/lua/pluginlist.lua
+++ home/.config/nvim/lua/pluginlist.lua
@@ -25,4 +25,31 @@ return {
 			})
 		end,
 	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+	},
+	{
+		"nvim-treesiter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"folke/neodev.nvim",
 }
