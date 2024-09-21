e41f99bfe176bad88f9209a25073d9a132dbe1b2
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2024-09-20 18:06:47 -0700

    Alphabetize tree-sitter parsers

--- home.nix
+++ home.nix
@@ -268,12 +268,12 @@
       {
         plugin = (
           nvim-treesitter.withPlugins (p: [
-            p.tree-sitter-nix
-            p.tree-sitter-vim
             p.tree-sitter-bash
-            p.tree-sitter-lua
-            p.tree-sitter-python
             p.tree-sitter-json
+            p.tree-sitter-lua
+            p.tree-sitter-nix
+            p.tree-sitter-python
+            p.tree-sitter-vim
           ])
         );
       }
