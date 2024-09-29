0a0e3a52381b64c769b4d3b56bcd70cf8222509e
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2024-09-29 13:03:15 -0700

    Format options.lua

--- home/.config/nvim/options.lua
+++ home/.config/nvim/options.lua
@@ -12,10 +12,10 @@ vim.o.number = true
 
 vim.o.signcolumn = 'yes'
 
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.tabstop = 4      -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 0 -- Delete one space at a time
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.o.softtabstop = 0  -- Delete one space at a time
+vim.o.shiftwidth = 4   -- Number of spaces inserted when indenting
 
 vim.o.updatetime = 300
 
@@ -33,7 +33,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
       buffer = args.buf,
       callback = function()
         -- 4 + 5
-        vim.lsp.buf.format {async = false, id = args.data.client_id }
+        vim.lsp.buf.format { async = false, id = args.data.client_id }
       end,
     })
   end
