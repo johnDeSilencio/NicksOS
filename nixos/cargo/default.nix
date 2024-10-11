138d1834cad0389a803e14e30bc723c091e60b90
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2024-10-10 21:39:55 -0700

    Add utility scripts to replace rustup for switching between rust toolchains

--- nixos/cargo/default.nix
+++ nixos/cargo/default.nix
@@ -1,5 +1,24 @@
 { config, pkgs, ... }:
+let
+  rust-nightly = pkgs.rust-bin.selectLatestNightlyWith (t: t.default);
+  rust-stable = pkgs.rust-bin.stable.latest.default;
+
+  # Define a custom script for using Rust nightly
+  switch-rust-nightly = pkgs.writeScriptBin "swn" ''
+    #! ${pkgs.fish}/bin/fish
+    set -U RUSTC "${rust-nightly}/bin/rustc"
+    set -U CARGO "${rust-nightly}/bin/cargo"
+  '';
+
+  # Define a custom script for using Rust stable
+  switch-rust-stable = pkgs.writeScriptBin "sws" ''
+    #! ${pkgs.fish}/bin/fish
+    set -U RUSTC "${rust-stable}/bin/rustc"
+    set -U CARGO "${rust-stable}/bin/cargo"
+  '';
+in
 {
+
   environment.systemPackages = with pkgs; [
     bugstalker
     cargo-asm
@@ -25,5 +44,9 @@
     cargo-update
     cargo-watch
     trunk
+
+    # Used to switch between installed Rust versions
+    switch-rust-nightly
+    switch-rust-stable
   ];
 }
