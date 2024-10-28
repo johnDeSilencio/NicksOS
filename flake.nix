24848248ce80e9b4d4d05e74bf06a752d58eb146
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2024-10-28 07:59:02 -0700

    Change nixpkgs url to more bleeding-edge nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nick's OS flake config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     catppuccin.url = "github:catppuccin/nix";
