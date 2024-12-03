3118a4e157129ea0f9a946bc3caa67b4282e18b1
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2024-12-03 07:24:25 -0800

    Add harper LSP config to Neovim for grammar checking

--- home/.config/nvim/plugin/lsp.lua
+++ home/.config/nvim/plugin/lsp.lua
@@ -76,3 +76,27 @@ require("lspconfig").rust_analyzer.setup({
 		},
 	},
 })
+
+require("lspconfig").harper_ls.setup {
+	settings = {
+		["harper-ls"] = {
+			diagnosticSeverity = "hint",
+			spell_check = true,
+			spelled_numbers = true,
+			an_a = true,
+			sentence_capitalization = true,
+			unclosed_quotes = true,
+			wrong_quotes = true,
+			long_sentences = true,
+			repeated_words = true,
+			spaces = true,
+			matcher = true,
+			correct_number_suffix = true,
+			number_suffix_capitalization = true,
+			multiple_sequential_pronouns = true,
+			linking_verbs = true,
+			avoid_curses = true,
+			terminating_conjunctions = true
+		}
+	},
+}
