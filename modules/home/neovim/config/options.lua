8314b4db303d3a693c729b113efd6db409a530f2
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2025-02-18 07:28:47 -0800

    Move around tab settings

--- modules/home/neovim/config/options.lua
+++ modules/home/neovim/config/options.lua
@@ -10,10 +10,11 @@ vim.o.number = true
 
 vim.o.signcolumn = 'yes'
 
+-- Tab
 vim.o.tabstop = 4      -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 0  -- Delete one space at a time
+vim.o.softtabstop = 4  -- Delete fours spaces at a time
 vim.o.shiftwidth = 4   -- Number of spaces inserted when indenting
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces
 
 vim.o.updatetime = 300
 
