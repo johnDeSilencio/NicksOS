d87f4d9e2e79b080371cff241aadf5b0e1cb804f
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2025-02-17 21:32:17 -0800

    Simplify neovim config by installing all available grammars

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -117,53 +117,7 @@
         }
 
         {
-          plugin = (
-            nvim-treesitter.withPlugins (p: [
-              p.tree-sitter-asm
-              p.tree-sitter-bash
-              p.tree-sitter-c
-              p.tree-sitter-cpp
-              p.tree-sitter-csv
-              p.tree-sitter-diff
-              p.tree-sitter-dockerfile
-              p.tree-sitter-fish
-              p.tree-sitter-git_config
-              p.tree-sitter-git_rebase
-              p.tree-sitter-gitattributes
-              p.tree-sitter-gitignore
-              p.tree-sitter-go
-              p.tree-sitter-gomod
-              p.tree-sitter-gosum
-              p.tree-sitter-helm
-              p.tree-sitter-javascript
-              p.tree-sitter-json
-              p.tree-sitter-json5
-              p.tree-sitter-jsonc
-              p.tree-sitter-just
-              p.tree-sitter-latex
-              p.tree-sitter-llvm
-              p.tree-sitter-lua
-              p.tree-sitter-make
-              p.tree-sitter-markdown
-              p.tree-sitter-markdown_inline
-              p.tree-sitter-nix
-              p.tree-sitter-passwd
-              p.tree-sitter-proto
-              p.tree-sitter-python
-              p.tree-sitter-ron
-              p.tree-sitter-rust
-              p.tree-sitter-sql
-              p.tree-sitter-ssh_config
-              p.tree-sitter-strace
-              p.tree-sitter-toml
-              p.tree-sitter-typescript
-              p.tree-sitter-verilog
-              p.tree-sitter-vhdl
-              p.tree-sitter-vim
-              p.tree-sitter-xml
-              p.tree-sitter-yaml
-            ])
-          );
+          plugin = nvim-treesitter.withAllGrammars;
         }
       ];
     };
