f2a29060419c128d65af228513cf7ed9d1ea34ad
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2025-02-18 07:36:10 -0800

    Rearrange vim options and add a few new ones for UI config and for searching

--- modules/home/neovim/config/options.lua
+++ modules/home/neovim/config/options.lua
@@ -5,9 +5,6 @@ vim.g.maplocalleader = ' '
 
 vim.o.clipboard = 'unnamedplus'
 
-vim.o.number = true
--- vim.o.relativenumber = true
-
 vim.o.signcolumn = 'yes'
 
 -- Tab
@@ -16,9 +13,19 @@ vim.o.softtabstop = 4  -- Delete fours spaces at a time
 vim.o.shiftwidth = 4   -- Number of spaces inserted when indenting
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces
 
+-- UI config
+vim.o.number = true         -- Absolute line number
+vim.o.relativenumber = true -- Add relative numbers on the side as well
+vim.o.cursorline = true     -- Highlight cursor line
+vim.o.termguicolors = true  -- Enable 24-bit RGB color in the TUI
+
+-- Searching
+vim.o.hlsearch = false  -- Do not highlight matches
+vim.o.ignorecase = true -- Ignore case in searches by default
+vim.o.smartcase = true  -- But make it case sensitive if uppercase is used
+
 vim.o.updatetime = 300
 
-vim.o.termguicolors = true
 
 vim.o.mouse = 'a'
 
