6bd671e67ea87433705e0edbdacf4748d831ca24
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2025-02-19 06:53:43 -0800

    Set up HTML and CSS language servers

--- modules/home/neovim/config/plugins/lsp.lua
+++ modules/home/neovim/config/plugins/lsp.lua
@@ -26,6 +26,8 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
+require("lspconfig").html.setup({ capabilities = capabilities })
+require("lspconfig").cssls.setup({ capabilities = capabilities })
 require("lspconfig").rust_analyzer.setup({ capabilities = capabilities })
 
 require("neodev").setup()
