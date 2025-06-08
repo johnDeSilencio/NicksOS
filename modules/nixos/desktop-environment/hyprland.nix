ee2959349343e9c0c191e4b30f93622176e59456
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2025-06-07 18:51:46 -0700

    Install hyprhook-mouse-move package to trigger waybar on mouse moving to the top of the screen

--- modules/nixos/desktop-environment/hyprland.nix
+++ modules/nixos/desktop-environment/hyprland.nix
@@ -2,8 +2,24 @@
   lib,
   config,
   pkgs,
+  inputs,
+  hyprhook,
+  hyprhook-mouse-move,
   ...
 }:
+let
+  hyprPluginPkgs = inputs.hyprland-plugins.packages.${pkgs.system};
+  hyprhookPkg = hyprhook.packages.${pkgs.system}.hyprhook;
+  hyprhookMouseMovePkg = hyprhook-mouse-move.packages.${pkgs.system}.default;
+  hypr-plugin-dir = pkgs.symlinkJoin {
+    name = "hyrpland-plugins";
+    paths = with hyprPluginPkgs; [
+      hyprexpo
+      hyprhookPkg
+      #...plugins
+    ];
+  };
+in
 {
   config = lib.mkIf config.custom.desktop-environment.enable {
     programs.hyprland = {
@@ -11,10 +27,17 @@
       xwayland.enable = true;
     };
 
+    # To communicate to Hyprland at runtime where the plugins are located
+    environment.sessionVariables = {
+      HYPR_PLUGIN_DIR = hypr-plugin-dir;
+    };
+
+    # Compile Rust binary for efficiently checking mouse position and showing / hiding waybar
     environment.systemPackages = with pkgs; [
       hypridle
       hyprpaper
       hyprshot
+      hyprhookMouseMovePkg
 
       # BUG: Screensharing in discord through xwaylandvideobridge
       # only works on built-in monitor. Also, if I spend too much time
