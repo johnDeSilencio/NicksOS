4efe057072a1512a4d9335637ec1ee3167799c0c
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2025-10-28 09:02:40 -0700

    Add every NerdFont from nixpkgs that I could get my hands on

--- modules/nixos/fonts/default.nix
+++ modules/nixos/fonts/default.nix
@@ -16,12 +16,67 @@
 
       # The home.packages option allows you to install Nix packages into your
       # environment.
-      home.packages = [
-        # # It is sometimes useful to fine-tune packages, for example, by applying
-        # # overrides. You can do that directly here, just don't forget the
-        # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-        # # fonts?
-        pkgs.nerd-fonts.intone-mono
+      home.packages = with pkgs; [
+        # Mono fonts
+        nerd-fonts.adwaita-mono
+        nerd-fonts.aurulent-sans-mono
+        nerd-fonts.blex-mono
+        nerd-fonts.caskaydia-mono
+        nerd-fonts.comic-shanns-mono
+        nerd-fonts.commit-mono
+        nerd-fonts.daddy-time-mono
+        nerd-fonts.dejavu-sans-mono
+        nerd-fonts.departure-mono
+        nerd-fonts.droid-sans-mono
+        nerd-fonts.fantasque-sans-mono
+        nerd-fonts.fira-code
+        nerd-fonts.fira-mono
+        nerd-fonts.geist-mono
+        nerd-fonts.intone-mono
+        nerd-fonts.jetbrains-mono
+        nerd-fonts.martian-mono
+        nerd-fonts.recursive-mono
+        nerd-fonts.roboto-mono
+        nerd-fonts.shure-tech-mono
+        nerd-fonts.ubuntu-mono
+        nerd-fonts.victor-mono
+        nerd-fonts.zed-mono
+
+        # Sans fonts
+        nerd-fonts.ubuntu-sans
+
+        # Other
+        nerd-fonts._3270
+        nerd-fonts.agave
+        nerd-fonts.anonymice
+        nerd-fonts.arimo
+        nerd-fonts.bigblue-terminal
+        nerd-fonts.code-new-roman
+        nerd-fonts.d2coding
+        nerd-fonts.envy-code-r
+        nerd-fonts.hasklug
+        nerd-fonts.heavy-data
+        nerd-fonts.hurmit
+        nerd-fonts.im-writing
+        nerd-fonts.inconsolata
+        nerd-fonts.inconsolata-go
+        nerd-fonts.iosevka
+        nerd-fonts.iosevka-term
+        nerd-fonts.iosevka-term-slab
+        nerd-fonts.lekton
+        nerd-fonts.liberation
+        nerd-fonts.lilex
+        # nerd-fonts.m+
+        nerd-fonts.meslo-lg
+        nerd-fonts.mononoki
+        nerd-fonts.noto
+        nerd-fonts.open-dyslexic
+        nerd-fonts.overpass
+        nerd-fonts.profont
+        nerd-fonts.sauce-code-pro
+        nerd-fonts.terminess-ttf
+        nerd-fonts.tinos
+        nerd-fonts.ubuntu
       ];
     };
   };
