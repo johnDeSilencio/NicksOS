b1299b247dfc66d0528e057eb7eb81d6506646dc
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2026-07-22 17:59:35 -0700

    Add extension for removing YouTube shorts from YouTube

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -84,6 +84,12 @@
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
             installation_mode = "force_installed";
           };
+
+          # For disabling YouTube Shorts
+          "{88ebde3a-4581-4c6b-8019-2a05a9e3e938}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/hide-youtube-shorts/latest.xpi";
+            installation_mode = "force_installed";
+          };
         };
 
         Preferences = {
