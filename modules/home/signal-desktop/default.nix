7016b0d0d4148d7601e1a0adea6ce9b1470097e0
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2026-07-22 23:24:26 -0700

    Reinstall signal

--- modules/home/signal-desktop/default.nix
+++ modules/home/signal-desktop/default.nix
@@ -11,8 +11,7 @@
 
   config = lib.mkIf config.custom.home.signal-desktop.enable {
     home-manager.users.nicholas.home.packages = with pkgs; [
-      # TODO: https://nixpk.gs/pr-tracker.html?pr=536273
-      # signal-desktop
+      signal-desktop
     ];
   };
 }
